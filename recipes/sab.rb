611f7045c67946e7688de3285781090f8e32d2c8
dantidote <dantidote@gmail.com>
2017-11-20 08:57:43 -0500

    start sab container

--- recipes/sab.rb
+++ recipes/sab.rb
@@ -11,3 +11,12 @@ docker_image 'dantidote/sab' do
   source '/root/docker_sab'
   action :build_if_missing
 end
+
+docker_container 'sab' do
+  repo 'dantidote/sab'
+  user '1004'
+  group '1111'
+  volumes ['/media/storage/Torrent/:/downloads', '/etc/letsencrypt/:/ssl', '/media/storage/docker/sab/:/config', '/media/storage/Torrent/Incomplete/:/incomplete']
+  port '8080:8080'
+  restart_policy 'always'
+end
