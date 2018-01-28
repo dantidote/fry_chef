6605593b667d758f88bbdf73641849ebed9f7ad1
unknown <dantidote@gmail.com>
2018-01-28 10:09:32 -0500

    couchpotato docker

---
+++ recipes/couchpotato.rb
@@ -0,0 +1,18 @@
+#
+# Cookbook:: fry_chef
+# Recipe:: couchpotato
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
+
+package 'docker'
+
+docker_image 'linuxserver/couchpotato'
+
+docker_container 'couchpotato' do
+  repo 'linuxserver/couchpotato'
+  tag 'latest'
+  env ['PUID=1003', 'PGID=1111', 'TZ=America/Detroit']
+  volumes ['/media/storage/Torrent/:/downloads', '/etc/letsencrypt/:/ssl', '/media/storage/Movies/:/movies', '/home/couchpotato/.couchpotato:/config']
+  port '8082:8082'
+  restart_policy 'always'
+end
