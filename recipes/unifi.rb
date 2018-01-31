7ec025c74a798602406eb736eddb30d2346a3b7b
unknown <dantidote@gmail.com>
2018-01-30 21:26:22 -0500

    unifi controller as docker

--- recipes/unifi.rb
+++ recipes/unifi.rb
@@ -4,25 +4,6 @@
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
 
-apt_repository 'unifi' do
-  uri 'http://www.ubnt.com/downloads/unifi/debian'
-  key '06E85760C0A52C50'
-  keyserver 'keyserver.ubuntu.com'
-  distribution 'stable'
-  components ['ubiquiti']
-end
-
-package 'unifi'
-
-#service 'unifi' do
-#  action [:enable, :start]
-#end
-
-template '/usr/lib/unifi/data/system.properties' do
-  source 'unifi.erb'
-  mode '644'
-end
-
 
 docker_image 'jacobalberty/unifi'
 
