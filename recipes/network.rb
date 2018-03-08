5f2236f30f2a415af6e6ca9bafc52bc717babfe3
unknown <dantidote@gmail.com>
2018-03-07 21:30:18 -0500

    modify network

--- recipes/network.rb
+++ recipes/network.rb
@@ -4,7 +4,7 @@
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
 
-ifconfig '10.0.1.2' do
+ifconfig '10.0.1.3' do
   action [:enable,:add]
 	device 'eno1'
   bootproto 'dhcp'
@@ -12,7 +12,7 @@ ifconfig '10.0.1.2' do
 end
 
 
-ifconfig '10.0.1.3' do
+ifconfig '10.0.1.2' do
   action [:enable,:add]
   device 'enp4s0'
   bootproto 'dhcp'
