036518fc3db78b73d7a8beba66e7631d77171ebb
unknown <dantidote@gmail.com>
2018-03-17 17:18:54 -0400

    move storj mounts to storj recipe

--- recipes/storj.rb
+++ recipes/storj.rb
@@ -4,6 +4,18 @@
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
 
+mount '/media/ext1' do
+  device '9c3895d9-e067-442b-88bf-1ca356454e43'
+  device_type :uuid
+  action [:mount, :enable]
+end
+
+mount '/media/ext2' do
+  device '4675e347-32a6-4403-8228-714e1cc26ed4'
+  device_type :uuid
+  action [:mount, :enable]
+end
+
 package 'docker'
 
 docker_image 'dantidote/storj' do
