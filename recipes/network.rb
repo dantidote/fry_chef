3c3117e2361323c57095e0c0b349c8b06b0f7dfa
unknown <dantidote@gmail.com>
2018-03-27 19:04:56 -0400

    10G NIC

--- recipes/network.rb
+++ recipes/network.rb
@@ -8,13 +8,16 @@ ifconfig '10.0.1.3' do
   action [:enable,:add]
 	device 'eno1'
   bootproto 'dhcp'
+  metric '1'
   onboot "yes"
 end
 
 
 ifconfig '10.0.1.2' do
   action [:enable,:add]
-  device 'enp4s0'
+  device 'enp2s0'
   bootproto 'dhcp'
+  metric '0'
+  mtu '9000'
   onboot "yes"
 end
