4e9b90fe475542e207bafd9f8de35ffb23ef8af6
unknown <dantidote@gmail.com>
2018-03-27 19:08:58 -0400

    new graphite docker image

--- recipes/graphite.rb
+++ recipes/graphite.rb
@@ -7,9 +7,9 @@
 package 'docker'
 
 docker_container 'graphite' do
-  repo 'sitespeedio/graphite'
+  repo 'graphiteapp/graphite-statsd'
   volumes ['/home/dan/docker/graphite/storage/whisper:/opt/graphite/storage/whisper',
            '/media/storage/docker/graphite/storage-schemas.conf:/opt/graphite/conf/storage-schemas.conf']
-  port ['8085:80', '2003:2003', '2003:2003/udp']
+  port ['8085:80', '2003-2004:2003-2004', '2023-2024:2023-2024', '8125:8125/udp', '8126:8126']
   restart_policy 'always'
 end
