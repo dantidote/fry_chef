72dc3e306cdc281253718ba7db355d7949fd03f4
unknown <dantidote@gmail.com>
2018-10-24 21:51:43 -0400

    cap memory do unifi

--- recipes/docker.rb
+++ recipes/docker.rb
@@ -4,7 +4,16 @@
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
 
-package 'docker'
+case node['platform']
+when 'raspbian'
+  execute 'get-docker' do
+    command 'curl -fsSL get.docker.com -o get-docker.sh && sh get-docker.sh'
+    not_if { File.exists?('/usr/bin/docker') }
+  end
+  package 'docker-ce'
+else
+  package 'docker'
+end
 
 service 'docker' do
   action [:enable, :start]
--- recipes/unifi.rb
+++ recipes/unifi.rb
@@ -4,7 +4,6 @@
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
 
-
 docker_image 'jacobalberty/unifi'
 
 docker_container 'unifi' do
@@ -12,4 +11,5 @@ docker_container 'unifi' do
   port ['8443:8443','3478:3478/udp','10001:10001/udp','8080:8080','8843:8843','8880:8880','6789:6789']
   env 'TZ=America/Detroit'
   volumes ['/media/storage/docker/unifi:/unifi']
+  memory '1073741824'
 end
