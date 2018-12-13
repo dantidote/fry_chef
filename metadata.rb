25695d04594f895ee96b590117c1c4d0ae63bcd9
dantidote <dantidote@gmail.com>
2018-12-13 09:24:19 -0500

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'dantidote@gmail.com'
 license 'All Rights Reserved'
 description 'Installs/Configures fry'
 long_description 'Installs/Configures fry'
-version '0.3.1'
+version '0.3.2'
 chef_version '>= 12.1' if respond_to?(:chef_version)
 
 depends 'resolver', '~> 2.0'
